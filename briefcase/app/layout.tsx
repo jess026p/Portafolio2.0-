4d2d9b06d7bcf6ce07e0174d16a2470b1072c063
jess026p <67206641+jess026p@users.noreply.github.com>
2024-06-16 09:35:03 -0500

    instalacion de swiper
    instalacion de swiper y importacion de librerias

--- briefcase/app/layout.tsx
+++ briefcase/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import "./globals.css";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 import Navbar from "@/components/navbar";
 import Header from "@/components/header";
 
