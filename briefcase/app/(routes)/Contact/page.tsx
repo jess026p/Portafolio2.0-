7f5d4c187f293f88ebe49c3bd9cb2aeffe672c3d
jess026p <67206641+jess026p@users.noreply.github.com>
2024-06-17 09:34:36 -0500

    Verificacion de mensajes

--- briefcase/app/(routes)/Contact/page.tsx
+++ briefcase/app/(routes)/Contact/page.tsx
@@ -7,93 +7,106 @@ import { FaUser, FaEnvelope, FaComment, FaPhone } from 'react-icons/fa'; // Impo
 
 const Page = () => {
 
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData);
 
-return (
-    <>
-        <TransitionPage />
-        <CoverParticles/>
-        <ContainerPage>
-            <div className="flex justify-center items-center h-full">
-                <div className="bg-secondary cardM shadow-md rounded-lg p-6 max-w-sm w-full">
-                    <h1 className="text-3xl font-bold mb-6 text-center text-white-800">Contactame</h1>
-                    <form  className="text-center">
-                    <div className="mb-3">
-                        <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
-                            <FaUser className="inline-block text-gray-400 mx-2" /> {/* Icono de usuario */}
-                            <input
-                                type="text"
-                                id="name"
-                                name="name"
-                                required
-                                className="bg-secondary rounded-md w-full py-2 px-3 focus:outline-none text-white-800" // Añadir color al texto
-                                placeholder="Nombre"
-                               
-                            />
-                        </div>
+        try {
+            const res = await fetch('/api/send', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            const result = await res.json();
+            console.log(result);
+            if (res.status === 200) {
+                alert("Email enviado con éxito");
+            } else {
+                alert("Error al enviar el email");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            alert("Error al enviar el email");
+        }
+    };
+
+    return (
+        <>
+            <TransitionPage />
+            <CoverParticles />
+            <ContainerPage>
+                <div className="flex justify-center items-center h-full">
+                    <div className="bg-secondary cardM shadow-md rounded-lg p-6 max-w-sm w-full">
+                        <h1 className="text-3xl font-bold mb-6 text-center text-white-800">Contactame</h1>
+                        <form onSubmit={handleSubmit} className="text-center">
+                            <div className="mb-3">
+                                <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                                    <FaUser className="inline-block text-gray-400 mx-2" />
+                                    <input
+                                        type="text"
+                                        id="name"
+                                        name="name"
+                                        required
+                                        className="bg-secondary rounded-md w-full py-2 px-3 focus:outline-none text-white-800"
+                                        placeholder="Nombre"
+                                    />
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                                    <FaEnvelope className="inline-block text-gray-400 mx-2" />
+                                    <input
+                                        type="email"
+                                        id="email"
+                                        name="email"
+                                        required
+                                        className="bg-secondary w-full py-2 px-3 rounded-md focus:outline-none text-white-800"
+                                        placeholder="Correo Electrónico"
+                                    />
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                                    <FaPhone className="inline-block text-gray-400 mx-2" />
+                                    <input
+                                        type="tel"
+                                        id="phone"
+                                        name="phone"
+                                        required
+                                        className="bg-secondary w-full py-2 px-3 rounded-md focus:outline-none text-white-800"
+                                        placeholder="Número de Teléfono"
+                                    />
+                                </div>
+                            </div>
+                            <div className="mb-3">
+                                <div className="bg-secondary messageC flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                                    <FaComment className="inline-block text-gray-400 mx-2" />
+                                    <textarea
+                                        id="message"
+                                        name="message"
+                                        required
+                                        className="bg-secondary messageCC w-full py-2 px-3 rounded-md focus:outline-none text-white"
+                                        placeholder="Mensaje"
+                                        rows={4}
+                                    ></textarea>
+                                </div>
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300"
+                            >
+                                Enviar
+                            </button>
+                        </form>
                     </div>
-                    <div className="mb-3">
-                        <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
-                            <FaEnvelope className="inline-block text-gray-400 mx-2" /> {/* Icono de correo */}
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                required
-                                className="bg-secondary w-full py-2 px-3 rounded-md focus:outline-none text-white-800" // Añadir color al texto
-                                placeholder="Correo Electrónico"
-                                
-                            />
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
-                            <FaPhone className="inline-block text-gray-400 mx-2" /> {/* Icono de teléfono */}
-                            <input
-                                type="tel"
-                                id="phone"
-                                name="phone"
-                                required
-                                className="bg-secondary w-full py-2 px-3 rounded-md focus:outline-none text-white-800" // Añadir color al texto
-                                placeholder="Número de Teléfono"
-                               
-                            />
-                        </div>
-                    </div>
-                    <div className="mb-3">
-                        <div className="bg-secondary messageC flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
-                            <FaComment className="inline-block text-gray-400 mx-2" /> {/* Icono de mensaje */}
-                            <textarea
-                                id="message"
-                                name="message"
-                                required
-                                className="bg-secondary messageCC w-full py-2 px-3 rounded-md focus:outline-none text-white" // Añadir color al texto
-                                placeholder="Mensaje"
-                                rows={4}
-                              
-                            ></textarea>
-                        </div>
-                    </div>
-                        <button onClick={async()=>{
-                           const res= await fetch('/api/send',{
-                                method:'POST',
-                                
-                            })
-                            const data= await res.json()
-                            console.log(data)
-                        }}
-                
-                           
-                            className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300"
-                            // Deshabilitar el botón mientras se carga
-                        >Enviar
-                        </button>
-                    </form>
-                    
                 </div>
-            </div>
-        </ContainerPage>
-    </>
-);
+            </ContainerPage>
+        </>
+    );
 };
 
 export default Page;
--- briefcase/app/api/send/route.ts
+++ briefcase/app/api/send/route.ts
@@ -2,22 +2,24 @@ import { Resend } from 'resend';
 import { EmailTemplate } from '@/components/email-template';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend('re_RnkhbJBm_GbDuqU4ojsffVzEB1zHFExqT')
- export async function POST() {
-  try {
-    const data = resend.emails.send({
-        from: 'Acme <onboarding@resend.dev>',
-        to: ['jesifv1999@gmail.com'],
-        subject: 'Probando resend desde mi portafolio ',
-        react: EmailTemplate({ firstName: 'Jesi09:14' }),
-        text:'Holaaaaaaaaaaaaaaa'
+const resend = new Resend('re_RnkhbJBm_GbDuqU4ojsffVzEB1zHFExqT');
 
-    })
-    console.log(data)
-    return NextResponse.json({message:"Email Sent"},{
-        status:200
-    })
+export async function POST(req) {
+  try {
+    const { name, email, phone, message } = await req.json();
+
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['jesifv1999@gmail.com'],
+      subject: `Nuevo mensaje de contacto de ${name}`,
+      react: EmailTemplate({ firstName: name }),
+      text: `Nombre: ${name}\nCorreo Electrónico: ${email}\nTeléfono: ${phone}\nMensaje: ${message}`,
+    });
+
+    console.log(data);
+    return NextResponse.json({ message: "Email Sent" }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({message:"Error"},{status:500})
+    console.error(error);
+    return NextResponse.json({ message: "Error" }, { status: 500 });
   }
 }
