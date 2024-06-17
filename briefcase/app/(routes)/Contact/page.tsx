99d84756f97ee986cd0701b33be71b317d114a1a
jess026p <67206641+jess026p@users.noreply.github.com>
2024-06-17 09:28:23 -0500

    prueba resend email

--- briefcase/app/(routes)/Contact/page.tsx
+++ briefcase/app/(routes)/Contact/page.tsx
@@ -1,121 +1,99 @@
-'use client';
+'use client'
 import TransitionPage from "@/components/transition-page";
 import ContainerPage from "@/components/container";
-import React, { useState } from 'react';
+import CoverParticles from '@/components/cover-particles';
+
 import { FaUser, FaEnvelope, FaComment, FaPhone } from 'react-icons/fa'; // Importa los iconos necesarios
 
 const Page = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        message: '',
-    });
 
-    // Definir el tipo del evento en handleChange
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
-    };
 
-    // Definir el tipo del evento en handleSubmit
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
-        try {
-            const res = await fetch('/api/send', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            console.log(data);
-        } catch (error) {
-            console.error('Error al enviar el formulario:', error);
-        }
-    };
-
-    return (
-        <>
-            <TransitionPage />
-            <ContainerPage>
-                <div className="flex justify-center items-center h-full"> {/* Aplica las clases flex, justify-center e items-center para centrar vertical y horizontalmente */}
-                    <div className="bg-secondary max-w-md p-4 rounded-md">
-                        <h1 className="text-3xl font-bold mb-4">Formulario de Contacto</h1>
-                        <form onSubmit={handleSubmit} className="text-center">
-                            <div className="mb-4">
-                                <div className="flex items-center border border-secondary rounded-md focus:border-blue-500 focus:ring-blue-500">
-                                    <FaUser className="inline-block text-gray-400 mx-2" /> {/* Icono de usuario */}
-                                    <input
-                                        type="text"
-                                        id="name"
-                                        name="name"
-                                        required
-                                        className="rounded-md w-full focus:outline-none"
-                                        placeholder="Nombre"
-                                        value={formData.name}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <div className="flex items-center border border-secondary rounded-md focus:border-blue-500 focus:ring-blue-500">
-                                    <FaEnvelope className="inline-block text-gray-400 mx-2" /> {/* Icono de correo */}
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        className="w-full rounded-md focus:outline-none"
-                                        placeholder="Correo Electrónico"
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <div className="flex items-center border border-secondary rounded-md focus:border-blue-500 focus:ring-blue-500">
-                                    <FaPhone className="inline-block text-gray-400 mx-2" /> {/* Icono de teléfono */}
-                                    <input
-                                        type="tel"
-                                        id="phone"
-                                        name="phone"
-                                        className="rounded-md w-full focus:outline-none"
-                                        placeholder="Número de Teléfono"
-                                        value={formData.phone}
-                                        onChange={handleChange}
-                                    />
-                                </div>
-                            </div>
-                            <div className="mb-4">
-                                <div className="flex items-center border border-secondary rounded-md focus:border-blue-500 focus:ring-blue-500">
-                                    <FaComment className="inline-block text-gray-400 mx-2" /> {/* Icono de mensaje */}
-                                    <textarea
-                                        id="message"
-                                        name="message"
-                                        className="w-full rounded-md focus:outline-none"
-                                        placeholder="Mensaje"
-                                        rows={5}
-                                        value={formData.message}
-                                        onChange={handleChange}
-                                    ></textarea>
-                                </div>
-                            </div>
-                            <button
-                                type="submit"
-                                className="animate-pulse bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-colors duration-300"
-                            >
-                                Enviar Mensaje
-                            </button>
-                        </form>
+return (
+    <>
+        <TransitionPage />
+        <CoverParticles/>
+        <ContainerPage>
+            <div className="flex justify-center items-center h-full">
+                <div className="bg-secondary cardM shadow-md rounded-lg p-6 max-w-sm w-full">
+                    <h1 className="text-3xl font-bold mb-6 text-center text-white-800">Contactame</h1>
+                    <form  className="text-center">
+                    <div className="mb-3">
+                        <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                            <FaUser className="inline-block text-gray-400 mx-2" /> {/* Icono de usuario */}
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                                required
+                                className="bg-secondary rounded-md w-full py-2 px-3 focus:outline-none text-white-800" // Añadir color al texto
+                                placeholder="Nombre"
+                               
+                            />
+                        </div>
                     </div>
+                    <div className="mb-3">
+                        <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                            <FaEnvelope className="inline-block text-gray-400 mx-2" /> {/* Icono de correo */}
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                required
+                                className="bg-secondary w-full py-2 px-3 rounded-md focus:outline-none text-white-800" // Añadir color al texto
+                                placeholder="Correo Electrónico"
+                                
+                            />
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="bg-secondary flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                            <FaPhone className="inline-block text-gray-400 mx-2" /> {/* Icono de teléfono */}
+                            <input
+                                type="tel"
+                                id="phone"
+                                name="phone"
+                                required
+                                className="bg-secondary w-full py-2 px-3 rounded-md focus:outline-none text-white-800" // Añadir color al texto
+                                placeholder="Número de Teléfono"
+                               
+                            />
+                        </div>
+                    </div>
+                    <div className="mb-3">
+                        <div className="bg-secondary messageC flex items-center border border-gray-300 rounded-md focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-500">
+                            <FaComment className="inline-block text-gray-400 mx-2" /> {/* Icono de mensaje */}
+                            <textarea
+                                id="message"
+                                name="message"
+                                required
+                                className="bg-secondary messageCC w-full py-2 px-3 rounded-md focus:outline-none text-white" // Añadir color al texto
+                                placeholder="Mensaje"
+                                rows={4}
+                              
+                            ></textarea>
+                        </div>
+                    </div>
+                        <button onClick={async()=>{
+                           const res= await fetch('/api/send',{
+                                method:'POST',
+                                
+                            })
+                            const data= await res.json()
+                            console.log(data)
+                        }}
+                
+                           
+                            className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors duration-300"
+                            // Deshabilitar el botón mientras se carga
+                        >Enviar
+                        </button>
+                    </form>
+                    
                 </div>
-            </ContainerPage>
-        </>
-    );
+            </div>
+        </ContainerPage>
+    </>
+);
 };
 
 export default Page;
--- briefcase/app/api/send/route.ts
+++ briefcase/app/api/send/route.ts
@@ -2,31 +2,22 @@ import { Resend } from 'resend';
 import { EmailTemplate } from '@/components/email-template';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend('re_C9iqnveQ_hYBNEKADoZDQQ7bzddzJi2hm');
+const resend = new Resend('re_RnkhbJBm_GbDuqU4ojsffVzEB1zHFExqT')
+ export async function POST() {
+  try {
+    const data = resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['jesifv1999@gmail.com'],
+        subject: 'Probando resend desde mi portafolio ',
+        react: EmailTemplate({ firstName: 'Jesi09:14' }),
+        text:'Holaaaaaaaaaaaaaaa'
 
-interface FormData {
-    name: string;
-    email: string;
-    phone: string;
-    message: string;
-}
-
-export async function POST(request: Request) {
-    const formData: FormData = await request.json(); // Extraer los datos del formulario del cuerpo de la solicitud
-    const { name, email, phone, message } = formData; // Desestructurar los datos del formulario
-
-    try {
-        const data = await resend.emails.send({
-            from: 'Acme <onboarding@resend.dev>',
-            to: [email], // Utilizar el email del formulario
-            subject: 'Mensaje de contacto desde mi portafolio', // Asunto del correo
-            react: EmailTemplate({ firstName: name }), // Utilizar el nombre del formulario en el template de correo
-            text: `Nombre: ${name}\nEmail: ${email}\nTeléfono: ${phone}\nMensaje: ${message}` // Cuerpo del correo con los datos del formulario
-        });
-        console.log(data);
-        return NextResponse.json({ message: "Email Sent" }, { status: 200 });
-    } catch (error) {
-        console.error('Error al enviar el correo:', error);
-        return NextResponse.json({ message: "Error" }, { status: 500 });
-    }
+    })
+    console.log(data)
+    return NextResponse.json({message:"Email Sent"},{
+        status:200
+    })
+  } catch (error) {
+    return NextResponse.json({message:"Error"},{status:500})
+  }
 }
